9e1945329d077eb8a2bb6cf1d23debb7f2a17783
amaliaks123 <amaliaks@ece.auth.gr>
2025-04-25 22:05:14 +0300

    ram.v

---
+++ ram.v
@@ -0,0 +1,20 @@
+module DATA_MEMORY(
+input clk,
+input we,
+input oe, 
+input [8:0] addr,
+input [31:0] din,
+output reg [31:0] dout
+);
+
+reg [31:0] RAM [511:0];
+
+always @(posedge clk)
+begin
+if(we)
+RAM[addr] = din;
+  if(oe)
+dout = RAM[addr];
+end
+
+endmodule
